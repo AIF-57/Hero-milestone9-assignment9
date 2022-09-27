621a9157894101789625deede4ee620b7424b2d4
ariyan-islam <afislam57@gmail.com>
2022-09-27 08:40:00 +0600

    initial setup

---
+++ src/components/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <p className='text-3xl'>Welcome To Home Page</p>
+        </div>
+    );
+};
+
+export default Home;
